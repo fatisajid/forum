7aa79f677b52363943474d65dd8d2b261c91bbd9
fatisajid <fatimaezahraa.salah@gmail.com>
2024-09-19 16:45:49 +0200

    adminUser

--- app/Views/partials/head.php
+++ app/Views/partials/head.php
@@ -3,39 +3,45 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FORUM</title>
+    <title>Forum</title>
+    <script src="https://kit.fontawesome.com/f5a1d28d53.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="/public/css/style.css">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg  text-white mb-3" style="background-color: #e3f2fd">
-  <div class="container-fluid">
-  <a class="navbar-brand" href="">
-      <img src="/public/img/logo.png" alt="Logo " width="30" height="30" class="d-inline-block align-text-top">
-      forum
-    </a>
-    
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" href="/">Acceuil</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="/register">Inscription</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="/connection">Connexion</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="/logout">Deconnexion</a>
-        </li>
-        
-        <li class="nav-item">
-          <a class="nav-link active" href="/users">Utilisateur</a>
-        </li>
-      </ul>
+<nav class="navbar navbar-expand-lg navbar-light text-white pb-3">
+    <div class="container-fluid">
+        <a class="navbar-brand mt-2 mt-lg-0 text-white" href="/">
+            <img src="/public/img/logo.png" alt="Logo" class="imgLogo">
+            Forum
+        </a>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-white">
+            <?php
+            if(isset($_SESSION['user'])){
+                ?>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="/logout"><i class="fa-solid fa-circle-plus"></i> Deconnexion</a>
+                </li>
+                <?php
+                if($_SESSION['user']['role']== "Admin"){
+                    ?>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="/users"><i class="fa-solid fa-circle-plus"></i> Utilisateurs</a>
+                    </li>
+                    <?php
+                }
+            } else {
+            ?>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="/register"><i class="fa-solid fa-circle-plus"></i> Inscription</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="/connection"><i class="fa-solid fa-circle-plus"></i> Connexion</a>
+                </li>
+            <?php
+            }
+            ?>
+        </ul>
     </div>
-  </div>
 </nav>
 <div class="myBody">
